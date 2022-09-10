c50a4090e97fc19f0755728e3c5ae6e1e7fad637
77Monisha <monishachaurasia77@gmail.com>
2022-09-10 23:03:49 +0530

    initial commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,26 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from './Button';
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Credit cards offer protection you wouldn’t get when paying with good old fashioned cash. When you pay with cash, your money is often as good as gone. Many credit cards offer extended warrantees beyond the manufacturer’s warrantee. 
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+)
+
+
+export default CardDeal
